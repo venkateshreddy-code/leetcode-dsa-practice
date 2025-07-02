6d7bc0144bacb3ce6dca3df9a887d1c3b25b1df2
venkatesh reddy Ningam <178813027+Venky178@users.noreply.github.com>
2025-07-02 17:52:47 -0400

    Create TwoSum.java

---
+++ Arrays & Hashing/TwoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n= nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            int remaining = target - nums[i];
+            if(map.containsKey(remaining)){
+                    return new int[] {map.get(remaining),i};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
